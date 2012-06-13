b8446911dd2838e9339be3da1512e513cf302284
Ian Lai <ian@fyianlai.com>
2012-06-13 17:29:20 +0800

    Added some documentation and fixed a bug
    Documented most of the core methods.
    Fixed a bug in update() which occurs when multiple WHERE conditions are
    used.

--- mysqli.class.php
+++ mysqli.class.php
@@ -14,7 +14,7 @@ defined('HYPERION') or die('No direct script access.');
 /**
  * Hyperion MySQL Database Abstraction Layer
  *
- * Concept derived from Wordpress DB Class:
+ * Concept loosely based on WordPress database class:
  * http://codex.wordpress.org/Class_Reference/wpdb
  *
  * @package		Hyperion
@@ -32,6 +32,10 @@ class MySQLi_DBAL implements Database
 	{
 		try {
 			$this->connect($host, $username, $password, $database);
+			
+			/*
+			 * @link http://www.php.net/manual/en/intro.reflection.php
+			 */
 			$this->reflection = new ReflectionClass('mysqli_stmt');
 		} catch (Exception $e) {
 			$e->getMessage();
@@ -71,9 +75,17 @@ class MySQLi_DBAL implements Database
 		return $ret;
 	}
 
-	public function prepare ($stmt, Array $params, $types, $reference = true)
+	/*
+	 * Prepare an SQL statement for execution.
+	 * @link http://www.php.net/manual/en/mysqli-stmt.prepare.php
+	 *
+	 * @param string $query A single SQL statement to be prepared.
+	 * @param array $params An array containing values to be binded to their corresponding markers in $query (in the order of their position in the array)
+	 * @return object The MySQLi_DBAL class instance is returned allowing for a fluent interface (chaining methods).
+	 */
+	public function prepare ($query, Array $params, $types, $reference = true)
 	{
-		$this->result = $this->instance->prepare($stmt);
+		$this->result = $this->instance->prepare($query);
 		array_unshift($params, $types);
 
 		if ($reference)
@@ -85,32 +97,30 @@ class MySQLi_DBAL implements Database
 	}
 
 	/*
-		Prepared INSERT statement
-
-		create (
-			table_name,
-			array(
-				'insert_column_name' => $insert_value
-			),
-			'(i|d|s|b)'
-		)
+	 * Insert row(s) into a table through a prepared statement.
+	 *
+	 * @param string $table The name of the table to insert data into.
+	 * @param array $data Data to insert (in column => value pairs). Both $data columns and $data values should be "raw" (neither should be SQL escaped).
+	 * @param string $formats A string containing all the specifiers/data types to be mapped to each of the values in $data.
+	 * @return object The MySQLi_DBAL class instance is returned allowing for a fluent interface (chaining methods).
 	 */
 	public function create ($table, Array $data, $formats)
 	{
+		// Build INSERT statement
 		$specifiers = str_repeat('?,', count($data));
 		$query = "INSERT INTO `{$table}` (`".implode("`,`", array_keys($data))."`) VALUES (".rtrim($specifiers, ',').")";
 
+		// Prepare statement
 		$this->prepare($query, $data, $formats, true)->execute();
 		return $this;
 	}
 
 	/*
-	 * Returns data from result set
-	 * 
-	 * Original code from:
-	 * http://net.tutsplus.com/tutorials/php/the-problem-with-phps-prepared-statements/
+	 * Read a single variable or entire row(s) in a database through a result set.
+	 * @link http://net.tutsplus.com/tutorials/php/the-problem-with-phps-prepared-statements/ Original code from Tuts+ NET.
 	 *
-	 * read ($mode)
+	 * @param string $mode (Optional) One of three pre-defined selection and output types (all|row|var). Defaults to 'all' returning all selected rows.
+	 * @return mixed The entire result of a query in the form of either an associative array (all|row) or a single variable (var) depending on $mode.
 	 */
 	public function read ($mode = 'all')
 	{
@@ -153,30 +163,25 @@ class MySQLi_DBAL implements Database
 	}
 
 	/*
-		Prepared UPDATE statement
-	
-		update (
-			table_name,
-			array(
-				'update_column_name' => $update_value
-			),
-			array(
-				'where_column_name' => $where_value
-			),
-			'(i|d|s|b)'
-		)
+	 * Update row(s) in a table through a prepared statement.
+	 *
+	 * @param string $table The name of the table to update.
+	 * @param array $update Data to update (in column => value pairs). Both $update columns and $update values should be "raw" (neither should be SQL escaped).
+	 * @param array $where A named array of WHERE clauses (in column => value pairs). Multiple clauses will be joined with ANDs. Both $where columns and $where values should be "raw".
+	 * @param string $formats A string containing all the specifiers/data types to be mapped to each of the values in $update and $where.
+	 * @param boolean $limit (Optional) The maximum number of rows to update. All matched rows will be updated if $limit is false.
+	 * @return object The MySQLi_DBAL class instance is returned allowing for a fluent interface (chaining methods).
 	 */
 	public function update ($table, Array $update, Array $where, $formats, $limit = false)
 	{
+		// Build array of values to bind
 		$data = array_merge($update, array_values($where));
 
 		// Build SET statement
 		$update_query = implode('=?,', array_keys($update));
-		$update_query .= '=?';
 
 		// Build WHERE statement
-		$condition_query = implode('=?,', array_keys($where));
-		$condition_query .= '=?';
+		$condition_query = implode('=? && ', array_keys($where));
 
 		// (Optional) Build LIMIT statement
 		$limit_query = $limit ? 'LIMIT ?' : '';
@@ -185,12 +190,21 @@ class MySQLi_DBAL implements Database
 			$formats .= 'i';
 		}
 
-		$query = "UPDATE `{$table}` SET {$update_query} WHERE {$condition_query} {$limit_query}";
-		var_dump($query);
+		// Prepare statement
+		$query = "UPDATE `{$table}` SET {$update_query}=? WHERE {$condition_query}=? {$limit_query}";
 		$this->prepare($query, $data, $formats)->execute();
+
 		return $this;
 	}
 
+	/*
+	 * Reference all values in an array.
+	 * @link http://www.php.net/manual/en/mysqli-stmt.bind-param.php
+	 *
+	 * @param array $vars Data (in column => value pairs) to reference in a new array.
+	 * @return array An array containing referenced values (in column => value pairs) - e.g. &string.
+	 * @uses prepare() The invoked mysqli_stmt_bind_param() method requires parameters to be passed by reference.
+	 */
 	public function reference (Array $vars)
 	{
 		$refs = array();
